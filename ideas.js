947464d4eedceb7cdeb396f613f48e7ca639e47c
donjd <99505859+donjd@users.noreply.github.com>
2025-12-23 11:43:14 -0700

    Add comment about idea

--- ideas.js
+++ ideas.js
@@ -1,9 +1,10 @@
 //Budeting hours on project based on established fees
-// function getHoursPerPerson(percentOfWork, phaseFee, feeRate) {
-//   return (percentOfWOrk * phaseFee) / feeRate;
-// }
+function getHoursPerPerson(percentOfWork, phaseFee, feeRate) {
+  return (percentOfWOrk * phaseFee) / feeRate;
+}
 
-// const myHours = getHoursPerPerson(1, 7000, 140);
+const myHours = getHoursPerPerson(1, 7000, 140);
 
-// console.log(myHours);
-console.log('hi);
+console.log(myHours);
+
+//Create form that accepts each team member, their rate code (S, P, or D), the fee associated with the code (to be autopopulated), the perecentage of hours expected to work, and the total hours that person can work to stay in budget.
