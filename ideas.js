b600c1b044c3f082b2b284805fc4747a4b686b31
donjd <99505859+donjd@users.noreply.github.com>
2025-12-23 11:32:51 -0700

    Add getHoursPerPerson function
    Fix function to calculate hours per person based on project fees.

---
+++ ideas.js
@@ -0,0 +1,6 @@
+//Budeting hours on project based on established fees
+function getHoursPerPerson(percentOfWork, phaseFee, feeRate) {
+  return (percentOfWOrk * phaseFee) / feeRate;
+}
+
+console.log(getHoursPerPerson(1, 7000, 140);
